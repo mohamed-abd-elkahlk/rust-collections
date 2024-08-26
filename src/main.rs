ff5d126b5a956822251447d2d814bbc956ce169b
mohamed abd elkhalq <01147018091.m@gmail.com>
2024-08-27 00:12:55 +0300

    todo list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 #[allow(dead_code, unused_variables)]
 mod vectors;
+// TODO: add docs for all under:>)
+///* Slice
+///* Cow
+///* Rc
+///* Arc
+///* Weak
+///* Cell
+///* RefCell
+///* Option
+///* Ruselt
+///* HashMap
+///* Hashset
+///* BTreeMap
+///* BTreeSet
+///* VecDeque
+///* LinkedList
+///* BinaryHeap
+///* BTreeSet
 fn main() {
     println!("Hello, world!");
 }
